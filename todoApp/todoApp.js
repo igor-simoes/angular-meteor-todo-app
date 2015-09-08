5bd35b2225efc7693e9ed21807a677c867031949
igorsimoes <iigor.simoes@gmail.com>
2015-09-08 02:33:03 -0300

    form for addTask and removeAll

--- todoApp/todoApp.js
+++ todoApp/todoApp.js
@@ -1,14 +1,43 @@
-if (Meteor.isClient) {
-    angular.module('todoApp', ['angular-meteor']);
+Tasks = new Mongo.Collection("tasks");
 
-    angular.module('todoApp').controller('todoController', function($scope){
+if (Meteor.isClient) {
+    var myApp = angular.module('todoApp', ['angular-meteor']);
+
+    myApp.controller('todoController', function($scope, $meteor){
         $scope.app = "todoApp Meteor";
+        $scope.tasks = $meteor.collection(Tasks);
+
+        $scope.addTask = function(task){
+            Tasks.insert(angular.copy(task));
+            delete task;
+        };
+
+        $scope.removeAllTask = function(){
+            // Tasks.remove({});
+            Meteor.call('removeAllTask')
+        };
+
     });
 
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+      if (Tasks.find().count()===0){
+          var tasks = [
+              {name: "School", hour: "15:00", status: "Incomplete"},
+              {name: "Make Dinner", hour: "20:00", status: "Complete"}
+          ];
+          for (var i=0;i<tasks.length;i++){
+              Tasks.insert(tasks[i]);
+          }
+          console.log("populate Tasks Collection");
+      }
 
+      return Meteor.methods({
+          removeAllTask: function() {
+              return Tasks.remove({});
+          }
+      });
   });
 }
